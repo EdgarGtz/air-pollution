9e10d04a36c9200c143f4f779984ff0d9d83bbd1
Edgar Gutierrez <edgargu47@gmail.com>
2023-02-27 11:15:40 -0600

    first commit

---
+++ pages/home.py
@@ -0,0 +1,66 @@
+import dash
+from dash import Dash, html, dcc
+import plotly.express as px
+import pandas as pd
+import dash_bootstrap_components as dbc
+import os
+
+dash.register_page(__name__, path="/")
+
+
+# Map
+
+# Upload data.
+sensors = pd.read_csv("assets/sensors.csv")
+
+# Mapbox token
+token = os.environ['DB_PWD_TER']
+
+# Set map layout
+map_layout = dict(
+    mapbox={
+        'accesstoken': token,
+        'style': "light",
+        'zoom': 12,
+        'center': dict(lat=25.65409262897884, lon=-100.37682059704264)
+    },
+    showlegend=False,
+    margin={'l': 0, 'r': 0, 'b': 0, 't': 0},
+    modebar=dict(remove=["zoom", "toimage", "pan", "select", "lasso", "zoomin", "zoomout", "autoscale", "reset",
+                         "resetscale", "resetview"]),
+    hoverlabel_bgcolor="#000000"
+)
+
+sensors = px.scatter_mapbox(sensors, lat="lat", lon="lon", custom_data=["name", "id", "sensor_index"])
+
+sensors.update_traces(hovertemplate="<br>".join([
+    "Name: %{customdata[0]}",
+    "ID: %{customdata[1]}",
+    "Sensor Index %{customdata[2]}"
+    ])
+)
+
+sensors.update_layout(map_layout)
+
+# Page layout
+layout = dbc.Container([
+
+    dbc.Row(
+        dbc.Col(
+            dbc.Card([
+                dbc.CardHeader(
+                    "Aire Limpio",
+                    style={'background-color': '#FFFFFF', 'font-weight': 'bold'}
+                ),
+                dbc.CardBody(
+                    dcc.Graph(figure=sensors,
+                              config={'displaylogo': False},
+                              style = {"height": "80vh", "width": "100%"}
+                    )
+                )
+            ]),
+            lg=12, className='pt-4'
+        )
+    )
+
+])
